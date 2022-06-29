1595ac4f1ccee7ab393b946ee735a482826031cb
calvin-giese <cgiese@fieldsafesolutions.com>
2022-06-29 13:06:03 -0600

    feat: add hello world request on front and back end

---
+++ backend/src/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8081"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def home():
+    return {"message": "Hello, world!"}
---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      msg: '',
+    };
+  },
+  methods: {
+    getMessage() {
+      axios.get('http://localhost:8000')
+        .then((res) => {
+          this.msg = res.data.message;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+  },
+  created() {
+    this.getMessage();
+  },
+};
+</script>
